8e891f4ec89b95aede7421e4113e06c7eb7ead38
Matej Sobota <matej.sobota@globallogic.com>
2018-12-11 17:12:39 +0100

    mk--lesson2

---
+++ less2/sobota.matej/src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    //    psvm tab
+    public static void main(String[] args) {
+//        sout tab
+        System.out.println("matej sobota");
+    }
+
+}
+//na domacom kompe hello world
+//zalozit www.github.com
