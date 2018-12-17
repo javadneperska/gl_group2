8756ac2cdc23668a2f3574d0c8b97911b8899db1
MatejPliesovsky <matej.pliesovsky@globallogic.com>
2018-12-17 13:07:07 +0100

    Verify if input int is even/odd - using for

--- homework/less4/pliesovsky.matej/src/main/java/Main.java
+++ homework/less4/pliesovsky.matej/src/main/java/Main.java
@@ -2,22 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Enter first number: ");
-        Scanner keyboard = new Scanner(System.in);
-        int firstint = keyboard.nextInt();
-        if (firstint % 2 == 0) {
-            System.out.println("First number " + firstint + " is even.");
-        } else {
-            System.out.println("First number " + firstint + " is odd.");
+
+        //with for
+        for (int i = 0; i < 2; i++) {
+            System.out.print("Enter the number: ");
+            Scanner keyboard = new Scanner(System.in);
+            int firstint = keyboard.nextInt();
+            if (firstint % 2 == 0) {
+                System.out.println("The number " + firstint + " is even." + "\n");
+            } else {
+                System.out.println("The number " + firstint + " is odd."  + "\n");
+            }
+//            System.out.println("");
+            System.out.println("**********************"  + "\n");
         }
 
-        System.out.print("Enter second number: ");
-        int secondtint = keyboard.nextInt();
-        if (secondtint % 2 == 0) {
-            System.out.println("Second number " + secondtint + " is even.");
-        } else {
-            System.out.println("Second number " + secondtint + " is odd.");
-        }
     }
-
 }
