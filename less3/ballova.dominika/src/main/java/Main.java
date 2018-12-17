28ba6e273f4ead3143a441e72672b05bb0a08630
dominikaballova <dominika.ballova@globallogic.com>
2018-12-17 08:06:14 +0100

    bd update commit

--- less3/ballova.dominika/src/main/java/Main.java
+++ less3/ballova.dominika/src/main/java/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is lesson 3");
+        int x = 3;
+        int y = ++x;
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+
+        System.out.println("****************");
+        x = 3;
+        y = 0;
+        y = x++;
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+
     }
 }
+
