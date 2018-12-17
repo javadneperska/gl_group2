26b9f8202a42b76d3efc139cb843ebe0cb87d6c4
MatejPliesovsky <matej.pliesovsky@globallogic.com>
2018-12-17 07:06:18 +0100

    Less3 incre/decre trying

--- less3/pliesovsky.matej/src/main/java/Main.java
+++ less3/pliesovsky.matej/src/main/java/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Matej P.");
+
+        int x = 3;
+        int y = ++x;
+
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+
+        System.out.println("******************");
+        x = 3;
+        y = 0;
+        y = x++;
+
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
     }
 }
