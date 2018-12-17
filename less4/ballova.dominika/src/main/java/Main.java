fa48b8c146e79ead509e0d850ea64e5c87b023da
dominikaballova <dominika.ballova@globallogic.com>
2018-12-17 08:59:57 +0100

    second commit

--- less4/ballova.dominika/src/main/java/Main.java
+++ less4/ballova.dominika/src/main/java/Main.java
@@ -1,15 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        // this is example of skrateny logicky sucin
         int x = 3;
         int y = 7;
         int z = -1;
 
-        if (x == 3 && y == 7 && z == 0)
-        {
-                System.out.println(true);
-            }
-        else
+        if (x == 3 && y == 7 && z == 0) {
+            System.out.println(true);
+        } else {
             System.out.println(false);
+        }
+
+// this is modulo example
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println("i: " + i + " je parne");
+            } else {
+                System.out.println("i: " + i + " je neparne");
+            }
+        }
+
+        {
+            int i = -1234;
+            System.out.format("i = %-7ahoj%n", i);
+
+
+            Scanner sc = new Scanner(System.in);
+
+        }
+
     }
 }
