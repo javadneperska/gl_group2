e182f194cf0a1f414a4df8d2cc0cb85a1f2a454c
Miroslav Katrak <miroslav.katrak@globallogic.com>
2018-12-18 17:06:07 +0100

    fomart

--- less5/katrak.miroslav/src/main/java/sk/mka/gl/group2/less5/Main.java
+++ less5/katrak.miroslav/src/main/java/sk/mka/gl/group2/less5/Main.java
@@ -1,11 +1,17 @@
 package sk.mka.gl.group2.less5;
 
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
+        formatLine("Ahoj");
+        formatLine("som");
+        formatLine("na");
+        formatLine("hodine");
+        formatLine("java");
+    }
 
+    private static void formatLine(String s) {
+        System.out.format("|%6s|%n", s);
     }
 }
