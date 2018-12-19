4741eade8799570597915048898946700a2a0867
martin-leso-lesinsky <lesiak@gmail.com>
2018-12-19 08:47:54 +0100

    leso - 2

--- less6/lesinsky.martin/src/main/java/Main.java
+++ less6/lesinsky.martin/src/main/java/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+
+
         System.out.println("Jednoduchy matematicky program");
         System.out.println("+----------------------------+");
         System.out.println("1. nasobenie");
         System.out.println("2. delenie");
         System.out.println("3. odpocitavanie");
-
-
     }
 }
