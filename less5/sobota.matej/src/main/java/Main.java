7df05981c9db1fb4a80835a941a3ddc68136b7da
Matej Sobota <matej.sobota@globallogic.com>
2018-12-19 07:31:48 +0100

    left aligment text

--- less5/sobota.matej/src/main/java/Main.java
+++ less5/sobota.matej/src/main/java/Main.java
@@ -3,16 +3,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        String s = "Ahoj";
-        System.out.format("%9s%n",s);
-        s="som";
-        System.out.format("%9s%n",s);
-        s="na";
-        System.out.format("%9s%n",s);
-        s="hodine";
-        System.out.format("%9s%n",s);
-        s="java";
-        System.out.format("%9s%n",s);
+        formatLine("Ahoj");
+        formatLine("som");
+        formatLine("na");
+        formatLine("hodine");
+        formatLine("java");
 
+    }
+
+    private static void formatLine(String s) {
+        System.out.format("%9s%n", s);
     }
 }
