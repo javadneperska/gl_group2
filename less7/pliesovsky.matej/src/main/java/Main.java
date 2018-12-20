a1deb3a61938213149e6fff2d0d1d6b4678658bd
MatejPliesovsky <matej.pliesovsky@globallogic.com>
2018-12-20 16:16:11 +0100

    breaker() and continuer()

--- less7/pliesovsky.matej/src/main/java/Main.java
+++ less7/pliesovsky.matej/src/main/java/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
+        breaker();
+        continuer();
+
         boolean x = true;
         int y = 0;
         int z = 1;
@@ -22,6 +25,25 @@ public class Main {
         } else {
             w = 1;
         }
+    }
 
+    private static int continuer() {
+        for (int i = 0; i < 3; i++) {
+            if (i == 1) {
+                continue;
+            }
+            System.out.println("i: " + i);
+        }
+        return 1;
+    }
+
+    private static int breaker() {
+        for (int i = 0; i < 3; i++) {
+            if (i == 1) {
+                break;
+            }
+            System.out.println("i: " + i);
+        }
+        return 1;
     }
 }
