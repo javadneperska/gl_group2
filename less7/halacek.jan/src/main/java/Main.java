b6ec51a76f35d9a07ca1a16dfaeca99b5adbb91f
techwritersvk <halacek@yahoo.com>
2018-12-20 17:01:49 +0100

    jh - update

--- less7/halacek.jan/src/main/java/Main.java
+++ less7/halacek.jan/src/main/java/Main.java
@@ -1,9 +1,5 @@
 public class Main {
-
     public static void main(String[] args) {
-
-        breaker();
-        continuer();
 
         boolean x = true;
         int y = 0;
@@ -11,40 +7,21 @@ public class Main {
 
         if (x == true) {
             System.out.println("x: " + x);
-            y = 1;
+            y = -1;
         } else {
             System.out.println("x: " + x);
-            z = 1;
+            z = -1;
         }
 
+        // example no.1
         int w = (x == true) ? 2 : 1;
 
+        // example no.2
         if (x == true) {
             w = 2;
         } else {
             w = 1;
         }
+
     }
-
-    private static int continuer() {
-        for (int i = 0; i < 3; i++) {
-            if (i == 1) {
-                continue;
-            }
-            System.out.println("i: " + i);
-        }
-        return 1;
-    }
-
-    private static int breaker() {
-        for (int i = 0; i < 3; i++) {
-            if (i == 1) {
-                break;
-            }
-            System.out.println("i: " + i);
-        }
-        return 1;
-    }
-
-
 }
