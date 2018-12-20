a0a00267979d492d2fb5924706492d7c5a8fb9db
techwritersvk <halacek@yahoo.com>
2018-12-20 17:28:42 +0100

    jh - more on this later

---
+++ less7/halacek.jan/src/main/java/Exercise.java
@@ -0,0 +1,29 @@
+public class Exercise {
+
+    public static void main(String[] args){
+
+        Scanner sc = newScanner(System.in);
+        double firstNumber = sc.nextDouble();
+        System.out.println("v: " + firstNumber);
+
+        double secondNumber = sc.nextDouble();
+        System.out.println("vl: " + secondNumber);
+
+        double sum = firstNumber + secondNumber;
+        System.out.println("sum: " + sum);
+
+        double h1 = 2;
+        double h2 = 3;
+        double sum2 = h1 + h2;
+
+//    public static void main(String[]args){
+//
+//        Scanner sc = newScanner(System.in);
+//        double v = sc.nextDouble();
+//        System.out.println("v: " + v);
+//
+//        double vl = sc.nextDouble();
+//        System.out.println("vl: " + vl);
+//
+//        System.out.println(v + vl);
+}
