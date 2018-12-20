9a05043aee23c59463f0653d95507629438c016d
Miroslav Katrak <miroslav.katrak@globallogic.com>
2018-12-20 16:15:07 +0100

    change

--- less7/katrak.miroslav/src/main/java/sk/mka/gl/group2/less4/Main.java
+++ less7/katrak.miroslav/src/main/java/sk/mka/gl/group2/less4/Main.java
@@ -1,31 +1,29 @@
 package sk.mka.gl.group2.less4;
 
-
 public class Main {
 
     public static void main(String[] args) {
-
-        boolean x = true;
-        int y = 0;
-        int z = 1;
-
-        if (x == true) {
-            System.out.println("x: " + x);
-            y = -1;
-        } else {
-            System.out.println("x: " + x);
-            z = -1;
-        }
-
-//        1.
-        int w = (x == true) ? 2 : 1;
-
-//        2.
-        if (x == true) {
-            w = 2;
-        } else {
-            w = 1;
-        }
+        breaker();
+        continuer();
     }
 
+    private static int continuer() {
+        for (int i = 0; i < 3; i++) {
+            if (i == 1) {
+                continue;
+            }
+            System.out.println("i: " + i);
+        }
+        return 1;
+    }
+
+    private static int breaker() {
+        for (int i = 0; i < 3; i++) {
+            if (i == 1) {
+                break;
+            }
+            System.out.println("i: " + i);
+        }
+        return 1;
+    }
 }
