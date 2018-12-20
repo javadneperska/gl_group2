3ef234ef75468f6ab42860b35286e0f12f029692
Miroslav Katrak <miroslav.katrak@globallogic.com>
2018-12-20 15:11:30 +0100

    fomart

--- less5/katrak.miroslav/src/main/java/sk/mka/gl/group2/less5/Main.java
+++ less5/katrak.miroslav/src/main/java/sk/mka/gl/group2/less5/Main.java
@@ -4,14 +4,61 @@ package sk.mka.gl.group2.less5;
 public class Main {
 
     public static void main(String[] args) {
-        formatLine("Ahoj");
-        formatLine("som");
-        formatLine("na");
-        formatLine("hodine");
-        formatLine("java");
+
+        String yourDecision = getYourDecision(1);
+        System.out.println("yourDecision:" + yourDecision);
+
+        if(yourDecision.equalsIgnoreCase("nasobenie")){
+            System.out.println("som nasobil");
+        }
+
     }
 
     private static void formatLine(String s) {
         System.out.format("|%6s|%n", s);
+    }
+
+
+    /**
+     * nasobenie 1
+     * delenie 2
+     * odpocitavanie 3
+     *
+     * @param mathematicOperation
+     * @return
+     */
+    private static String getYourDecision(int   mathematicOperation) {
+        String operacia = "unknown";
+
+        switch (mathematicOperation) {
+            case 1:
+                operacia = "nasobenie";
+                break;
+            case 2:
+                operacia = "delenie";
+                break;
+            case 3:
+                operacia = "odpocitavanie";
+                break;
+
+            default:
+                operacia = "unknown";
+                break;
+        }
+
+
+
+        return operacia;
+    }
+
+
+    private boolean isTrueThat() {
+        int x = 3;
+
+        int xedo = (x == 3) ? 2 : 1;
+        System.out.println("xedo: " + xedo);
+
+
+        return true;
     }
 }
