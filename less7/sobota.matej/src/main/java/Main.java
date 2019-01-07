1d48d8b6bbc02ba1efd3cdc0ceffc73df54d3154
Matej Sobota <matej.sobota@globallogic.com>
2019-01-07 15:17:13 +0100

    pom comit

--- less7/sobota.matej/src/main/java/Main.java
+++ less7/sobota.matej/src/main/java/Main.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-
-
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner scanner = new Scanner(new File("text.csv"));
+
+        File file = new File("text.csv");
+        Scanner scanner = new Scanner(file);
         scanner.useDelimiter(",");
         while(scanner.hasNext()){
             System.out.print(scanner.next()+"|");
