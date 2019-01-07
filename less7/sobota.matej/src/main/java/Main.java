b529b096832f5edd808f8fea174fdef155d53d03
Matej Sobota <matej.sobota@globallogic.com>
2019-01-07 09:52:18 +0100

    pom comit

--- less7/sobota.matej/src/main/java/Main.java
+++ less7/sobota.matej/src/main/java/Main.java
@@ -1,46 +1,18 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        System.out.println("Enter first number: ");
-        int aa = reader.nextInt();
-        System.out.println("Enter second number: ");
-        int bb = reader.nextInt();
-
-        int a = 1;
-        int b = 2;
-
-        System.out.println("Sum of numbers is: " + (aa + bb + a + b));
-
-
-
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("text.csv"));
+        scanner.useDelimiter(",");
+        while(scanner.hasNext()){
+            System.out.print(scanner.next()+"|");
+        }
+        scanner.close();
     }
-
 
 //    program ktory z externaho suboru nacita cisla (min 10 )v formate csv nasledne ich vypise scita ich vynasobi a zisti najvecsie a najmensie
-
-
-    public static int breaker(){
-        for (int i = 0; i < 5; i++) {
-            if (i == 1){
-                break;
-            }
-            System.out.println("i: "+ i);
-        }
-        return 1;
-    }
-
-    public static int continuer(){
-        for (int i = 0; i < 5; i++) {
-            if (i == 1){
-                continue;
-            }
-            System.out.println("i: "+ i);
-        }
-        return 1;
-
-    }
-
-
 }
