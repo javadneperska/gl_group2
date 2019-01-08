8fb33c04baf0b4c8aa1fa8eb02b59de1eee05d67
Miroslav Katrak <miroslav.katrak@globallogic.com>
2019-01-08 18:01:04 +0100

    less9

--- less7/katrak.miroslav/src/main/java/sk/mka/gl/group2/less4/Main.java
+++ less7/katrak.miroslav/src/main/java/sk/mka/gl/group2/less4/Main.java
@@ -1,29 +1,30 @@
 package sk.mka.gl.group2.less4;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        breaker();
-        continuer();
+
+        Scanner sc = new Scanner(System.in);
+        double firstNumber = sc.nextDouble();
+        System.out.println("v: " + firstNumber);
+
+        double secondNumber = sc.nextDouble();
+        System.out.println("v1: " + secondNumber);
+
+        double sum = firstNumber + secondNumber;
+        System.out.println("sum: " + sum);
+
+
+        double h1 = 2;
+        double h2 = 3;
+        double sum2 = h1 + h2;
+
+        System.out.println("sum2:" + sum2);
+
+        System.out.println("final_sum: " + (sum + sum2));
+
     }
 
-    private static int continuer() {
-        for (int i = 0; i < 3; i++) {
-            if (i == 1) {
-                continue;
-            }
-            System.out.println("i: " + i);
-        }
-        return 1;
-    }
-
-    private static int breaker() {
-        for (int i = 0; i < 3; i++) {
-            if (i == 1) {
-                break;
-            }
-            System.out.println("i: " + i);
-        }
-        return 1;
-    }
 }
---
+++ less9/katrak.miroslav/src/main/java/sk/mka/gl/group2/less9/Main.java
@@ -0,0 +1,9 @@
+package sk.mka.gl.group2.less9;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
