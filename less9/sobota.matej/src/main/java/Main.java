843f7f2ed777719d27f712eb67457a0f8717fc4a
Matej Sobota <matej.sobota@globallogic.com>
2019-01-10 16:41:37 +0100

    fsf

--- less9/sobota.matej/src/main/java/Main.java
+++ less9/sobota.matej/src/main/java/Main.java
@@ -1,2 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(max(5,6));
+
+    }
+    static int max(int a, int b){
+        if (a > b){
+            return(a);
+        }
+        else return (b);
+
+    }
+
 }
