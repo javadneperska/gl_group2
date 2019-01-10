023b4441734a850c533dfdda3e333ed4ec56b135
Miroslav Katrak <miroslav.katrak@globallogic.com>
2019-01-10 18:10:16 +0100

    xxxx

--- less10/katrak.miroslav/src/main/java/sk/mka/gl/group2/less10/Main.java
+++ less10/katrak.miroslav/src/main/java/sk/mka/gl/group2/less10/Main.java
@@ -6,6 +6,15 @@ public class Main {
         Star star = new Star();
         star.printStar();
 
+        Star star1 = new Star();
+        star1.printStar();
+
+        Star star2 = new Star();
+        star2.printStar();
+
+        /*static method exmample */
         Util.printValue("som_hviezda");
+
+        System.out.println("Star.STAR_COUNTER: " + Star.STAR_COUNTER);
     }
 }
--- less10/katrak.miroslav/src/main/java/sk/mka/gl/group2/less10/Star.java
+++ less10/katrak.miroslav/src/main/java/sk/mka/gl/group2/less10/Star.java
@@ -2,6 +2,16 @@ package sk.mka.gl.group2.less10;
 
 public class Star {
 
+    public static int STAR_COUNTER;
+    public int starCount;
+
+    public Star() {
+        starCount++;
+        STAR_COUNTER++;
+    }
+
+    private int count;
+
     public void printStar() {
         System.out.println("*");
     }
