5639979cf5cae3aade4b58270bcbb1cbf13e701d
Miroslav Katrak <miroslav.katrak@globallogic.com>
2019-01-15 17:10:45 +0100

    xxx

---
+++ less11/katrak.miroslav/src/main/java/sk/mka/gl/group2/less11/Utils.java
@@ -0,0 +1,11 @@
+package sk.mka.gl.group2.less11;
+
+import java.util.Arrays;
+
+public class Utils {
+
+    public static void printArray(int a[]){
+        final String toString = Arrays.toString(a);
+        System.out.println("array: " + toString);
+    }
+}
