e1fc9d48970ed38d29972577fe98df98d37b887c
Miroslav Katrak <miroslav.katrak@globallogic.com>
2019-01-15 17:26:21 +0100

    sorting aded

--- less11/katrak.miroslav/src/main/java/sk/mka/gl/group2/less11/Main.java
+++ less11/katrak.miroslav/src/main/java/sk/mka/gl/group2/less11/Main.java
@@ -1,17 +1,23 @@
 package sk.mka.gl.group2.less11;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        int[] a = {1, 2, 3, 5, 7, 11};
+        int[] a = {3, 8, 11,4, 6, 12};
         int[] b = {1, 2, 3, 5, 7, 11};
-        Utils.printArray(a);
+
+        Arrays.sort(a);
+        Utils.printArray("po_sortovani_a", a);
         int sumA = sumArray(a);
-        Utils.printArray(a);
+        Utils.printArray("po_a", a);
+
         System.out.println("sumA: " + sumA);
-        Utils.printArray(b);
+        Arrays.sort(b);
+        Utils.printArray("po_sortovani_b", b);
         int sumB = sumArray(b);
-        Utils.printArray(b);
+        Utils.printArray("po_b", b);
 
         System.out.println("sum_together: " + (sumA + sumB));
     }
