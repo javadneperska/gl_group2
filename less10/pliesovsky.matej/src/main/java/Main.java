864fdcd889db8ff135e72d081f908d85761de80c
MatejPliesovsky <matej.pliesovsky@globallogic.com>
2019-01-15 16:04:24 +0100

    not complete from lesson

--- less10/pliesovsky.matej/src/main/java/Main.java
+++ less10/pliesovsky.matej/src/main/java/Main.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+       int IntCompare = new IntCompare();
 
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Enter your first number: ");
+        int a = keyboard.nextInt();
+
+        System.out.println("Enter your second string: ");
+        int b = keyboard.nextInt();
+
+        // Call compareStrings
+//        compareStrings();
+        IntCompare compare = new IntCompare();
+        compare.compareInts(a, b);
     }
 }
+
+
+class IntCompare {
+    String a = "";
+    String b = "";
+
+
+    // Create method to compare them, to be called in StringCompareTest
+    public static void compareInts(int a, int b) {
+        if (a.compareTo(b) > 0)
+            System.out.println("The first string is greater than the second.");
+        else if (a.compareTo(b) < 0)
+            System.out.println("The first string is smaller than the second.");
+        else
+            System.out.println("Both the strings are equal.");
+
+
+        }
+    }
+
