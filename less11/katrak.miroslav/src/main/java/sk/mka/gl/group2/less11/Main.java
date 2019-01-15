ade669427b8363aa0ce5a0bb3a21d60a71dda084
Miroslav Katrak <miroslav.katrak@globallogic.com>
2019-01-15 16:50:55 +0100

    11

--- less11/katrak.miroslav/src/main/java/sk/mka/gl/group2/less11/Main.java
+++ less11/katrak.miroslav/src/main/java/sk/mka/gl/group2/less11/Main.java
@@ -1,8 +1,12 @@
 package sk.mka.gl.group2.less11;
 
+import sk.mka.gl.group2.less10.Star;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("11");
+        Star star = new Star();
+        System.out.println("Star.STAR_COUNTER: " + Star.STAR_COUNTER);
     }
 }
