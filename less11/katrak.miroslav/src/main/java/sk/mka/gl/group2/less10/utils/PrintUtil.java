d211f46a95a2addecc7980c3427af1b5c219587a
Miroslav Katrak <miroslav.katrak@globallogic.com>
2019-01-15 16:39:20 +0100

    11

--- less11/katrak.miroslav/src/main/java/sk/mka/gl/group2/less10/utils/PrintUtil.java
+++
@@ -1,8 +0,0 @@
-package sk.mka.gl.group2.less10.utils;
-
-public class PrintUtil {
-
-    public static void printValue(String s) {
-        System.out.println(s);
-    }
-}
---
+++ less11/katrak.miroslav/src/main/java/sk/mka/gl/group2/less11/Main.java
@@ -0,0 +1,8 @@
+package sk.mka.gl.group2.less11;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("11");
+    }
+}
