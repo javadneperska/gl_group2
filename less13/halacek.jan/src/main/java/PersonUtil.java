0ec1f0d3cfb18ee0a2c5518988dfd73aa1b2f092
jan.halacek <halacek@yahoo.com>
2019-01-22 16:05:25 +0100

    ready to jive with java

---
+++ less13/halacek.jan/src/main/java/PersonUtil.java
@@ -0,0 +1,7 @@
+public class PersonUtil {
+
+    public static void printPerson(Person person){
+        System.out.println("firstName; " + person.getFirstName());
+        System.out.println("lastName; " + person.getLastName());
+    }
+}
