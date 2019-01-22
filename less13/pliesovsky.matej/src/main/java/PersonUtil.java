e9ffab5641f9b92459f458ba7d73aa5d2ea64a62
MatejPliesovsky <matej.pliesovsky@globallogic.com>
2019-01-22 16:29:11 +0100

    rectangle contents

---
+++ less13/pliesovsky.matej/src/main/java/PersonUtil.java
@@ -0,0 +1,9 @@
+public class PersonUtil {
+
+    public static void printPerson(Person person) {
+
+        System.out.println("Firstname: " + person.getFirstName());
+        System.out.println("Lastname: " + person.getLastName());
+
+    }
+}
---
+++ less13/pliesovsky.matej/src/main/java/geometry/Rectangle.java
@@ -0,0 +1,40 @@
+package geometry;
+
+public class Rectangle {
+    private double width;
+    private double height;
+
+    public Rectangle() {
+        this(1,1);
+    }
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public Rectangle(Rectangle rectangle) {
+        this(rectangle.width, rectangle.height);
+    }
+
+// metoda na vypocet obsahu rectangla
+    public double contents(){
+        return this.width * this.height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
