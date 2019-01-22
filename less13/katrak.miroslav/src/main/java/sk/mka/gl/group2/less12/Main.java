aed8ab420f74b350aa6fe83dd57d851af49aa37f
Miroslav Katrak <miroslav.katrak@globallogic.com>
2019-01-22 16:21:22 +0100

    xx

---
+++ less13/katrak.miroslav/src/main/java/sk/mka/gl/group2/less12/Main.java
@@ -0,0 +1,44 @@
+package sk.mka.gl.group2.less12;
+
+import sk.mka.gl.group2.less12.geomethry.Rectangle;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //3 times rectangle
+//        1.
+        Rectangle a = new Rectangle();
+        a.setWidth(3);
+        a.setHeight(4);
+
+//        2.
+        Rectangle b = new Rectangle(8, 9);
+
+        //3.
+        Rectangle c = new Rectangle(10, 12);
+
+
+       /* Rectangle r = new Rectangle();
+        System.out.println("r_height: " + r.getHeight());
+        System.out.println("r_width: " + r.getWidth());
+
+        Rectangle square = new Rectangle(r);
+        System.out.println("height: " + square.getHeight());
+        System.out.println("width: " + square.getWidth());
+
+
+        //father
+        Person m = new Person();
+        m.setFirstName("Miroslav");
+        m.setLastName("Katrak");
+
+        //son
+        Person ym = new Person("Miroslav", "Katrak");
+
+        PersonUtil.printPerson(m);
+        PersonUtil.printPerson(ym);*/
+
+
+    }
+}
---
+++ less13/katrak.miroslav/src/main/java/sk/mka/gl/group2/less12/geomethry/Rectangle.java
@@ -0,0 +1,36 @@
+package sk.mka.gl.group2.less12.geomethry;
+
+public class Rectangle {
+
+    private double width;
+    private double height;
+
+    public Rectangle() {
+        this(1, 1);
+    }
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public Rectangle(Rectangle rectangle) {
+        this(rectangle.width, rectangle.height);
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
