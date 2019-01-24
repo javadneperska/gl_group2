c0a4f0cd708e9b567cec522101e9094561e60255
Matej Sobota <matej.sobota@globallogic.com>
2019-01-24 16:08:02 +0100

    comit

---
+++ homework/less13/sobota.matej/src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+}
+
+
+//do systemu na pracovnu plochu ulozi zoznam zamestnancov GL minimalne o pocte 10...udaje nacita z CSV ktory bude ulozeny na desktope s nazvom input.csv
+//meno priezvisko datum nastupenie pocet rokov praxe a zoznam technologii a program vypise 3 nauspesnejsich
+//podla rokov praxe a usporiada ich podla priezviska a mena
