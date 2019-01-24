74b1413ee5f808101307e8eb0310b2281e4d71b1
jan.halacek <halacek@yahoo.com>
2019-01-24 15:53:53 +0100

    JH hereandnow

---
+++ less14/halacek.jan/src/main/java/Property.java
@@ -0,0 +1,20 @@
+public class Property {
+    String name;
+    boolean man;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isMan() {
+        return man;
+    }
+
+    public void setMan(boolean man) {
+        this.man = man;
+    }
+}
