81897464cbfd7b13e325905c2e21720f41516daa
MatejPliesovsky <matej.pliesovsky@globallogic.com>
2019-01-29 14:20:13 +0100

    HW from less13

--- homework/less13/pliesovsky.matej/src/main/java/Main.java
+++ homework/less13/pliesovsky.matej/src/main/java/Main.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+
 public class Main {
+    private static String DESKTOP_PATH = System.getProperty("user.home") + "/Desktop/";
+
     public static void main(String[] args) {
-        System.out.print(" Initial ");
+        ReadFUtils readFUtils = new ReadFUtils(",");
+        ArrayList<String[]> fileData = readFUtils.readFile(DESKTOP_PATH + "input.csv");
+        PersonEm[] personEms = new PersonEm[fileData.size()];
+        for (int i = 0; i < fileData.size(); i++) {
+            personEms[i] = new PersonEm(fileData.get(i));
+        }
+        PersonEm[] personSorter = UtilsE.sortBy(personEms, "stage");
+        UtilsE.printLastNElementsReversed(personSorter, 3);
+        StringBuilder dataToWrite = new StringBuilder();
+        for (PersonEm personEm : UtilsE.sortBy(personEms, "surname")) {
+            dataToWrite.append(personEm.toString()).append(System.getProperty("line.separator"));
+        }
+
+        readFUtils.writeToFile(DESKTOP_PATH + "output.txt", dataToWrite.toString());
     }
+
 }
---
+++ homework/less13/pliesovsky.matej/src/main/java/PersonEm.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PersonEm {
+    private String firstName;
+    private String lastName;
+    private String workEntryDate;
+    private int stage;
+    private ArrayList<String> workSkills = new ArrayList<String>();
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getWorkEntryDate() {
+        return workEntryDate;
+    }
+
+    public void setWorkEntryDate(String workEntryDate) {
+        this.workEntryDate = workEntryDate;
+    }
+
+    public int getStage() {
+        return stage;
+    }
+
+    public void setStage(int stage) {
+        this.stage = stage;
+    }
+
+    public ArrayList<String> getWorkSkills() {
+        return workSkills;
+    }
+
+    public void setWorkSkills(ArrayList<String> workSkills) {
+        this.workSkills = workSkills;
+    }
+
+    public PersonEm(String[] fileLine) {
+        this.firstName = fileLine[0];
+        this.lastName = fileLine[1];
+        this.workEntryDate = fileLine[2];
+        this.stage = Integer.parseInt(fileLine[3]);
+        this.workSkills.addAll(Arrays.asList(fileLine).subList(4, fileLine.length));
+    }
+
+    public void printEmployee(){
+        System.out.println(toString());
+    }
+
+    public String toString(){
+        return this.firstName + " " + this.lastName + " " + this.workEntryDate + " " + this.stage + " " +
+                this.workSkills.toString();
+    }
+
+}
