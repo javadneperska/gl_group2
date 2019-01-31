ccdee09c3002eda32a502b19d608e3781a8dd58c
MatejPliesovsky <matej.pliesovsky@globallogic.com>
2019-01-31 16:17:27 +0100

    less 16 init

--- less16/pliesovsky.matej/src/main/java/Main.java
+++ less16/pliesovsky.matej/src/main/java/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+        String s= "Som,na;hodine:javy";
+        String[] podretazce = s.split("[;:,<>]");
+        for (int i = 0; i < podretazce.length; i++) {
+            System.out.println(podretazce[i]);
+        }
+    }
 }
