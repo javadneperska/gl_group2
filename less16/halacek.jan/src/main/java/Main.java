d043902553f1ce1be2fb9e2e665701bf53f1273d
jan.halacek <halacek@yahoo.com>
2019-01-31 16:30:46 +0100

    jh - behold the beauty, folks

--- less16/halacek.jan/src/main/java/Main.java
+++ less16/halacek.jan/src/main/java/Main.java
@@ -1,21 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        String d = "Soma, na; hodine: Javy";
+        String d = "Sedim, na; paradnej> hodine: Javy";
 
-        String[] splittedString = d.split("[,;:]");
+        String[] splittedString = d.split("[,;>:]");
 
         for (String c :
                 splittedString) {
             System.out.println(c);
         }
 
-        Human oleg = new Human("Jan", "Halacek", new Date(30, 6, 1989));
-        Human moleg = new Human("Ola", "Bambola", new Date(11, 9, 1995));
-        Human zeleg = new Human("Tucker", "Ducker", new Date(31, 12, 2004));
+        Human jan = new Human("Jan", "Halacek", new Date(30, 6, 1989));
+        Human olala = new Human("Ola", "Bambola", new Date(11, 9, 1995));
+        Human pucker = new Human("Tucker", "Ducker", new Date(31, 12, 2004));
 
-        System.out.println(oleg);
-        System.out.println(moleg);
-        System.out.println(zeleg);
+        System.out.println(jan);
+        System.out.println(olala);
+        System.out.println(pucker);
+
+        Human[] humans = new Human[3];
+        for (int i = 0; i < humans.length; i++) {
+            humans[i] = new Human("whatever" + i, "whoever" + i, new Date(5,5,1955));
+        }
+
+        for (Human ccc :
+                humans) {
+            System.out.println(ccc);
+        }
     }
 }
 
