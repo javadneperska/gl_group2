4a0f24cb536ea5b762a04cc761410ecedee773fc
MatejPliesovsky <matej.pliesovsky@globallogic.com>
2019-02-04 08:18:11 +0100

    less 16 save

--- less16/pliesovsky.matej/src/main/java/Main.java
+++ less16/pliesovsky.matej/src/main/java/Main.java
@@ -5,5 +5,20 @@ public class Main {
         for (int i = 0; i < podretazce.length; i++) {
             System.out.println(podretazce[i]);
         }
+
+
+        Human clovek = new Human();
+        clovek.setFirstName("Matej");
+        clovek.setLastName("Pliesovsky");
+
+        System.out.println(clovek.toString());
+
+        Human jano = new Human("Jan", "Kovac", new Date(30, 6, 1989));
+        Human palo = new Human("Palo", "Halas", new Date(10, 8, 1995));
+        Human mato = new Human("Mato", "Haluska", new Date(30, 10, 2004));
+
+        System.out.println(jano);
+        System.out.println(palo);
+        System.out.println(mato);
     }
 }
