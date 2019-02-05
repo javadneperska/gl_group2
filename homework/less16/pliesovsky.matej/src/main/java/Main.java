f5d90166af6001886a2c5cbda94810fa7b297acc
MatejPliesovsky <matej.pliesovsky@globallogic.com>
2019-02-05 10:00:46 +0100

    less16 hw

--- homework/less16/pliesovsky.matej/src/main/java/Main.java
+++ homework/less16/pliesovsky.matej/src/main/java/Main.java
@@ -1,7 +1,14 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
-    private static String DESKTOP_PATH = System.getProperty("user.home") + "/Desktop/";
+
     public static void main(String[] args) {
+        CSVreader newCSV = new CSVreader();
+        ArrayList<String[]> csvItems = CSVreader.readFile();
+        for (int i=0; i < csvItems.size(); i++)
+        {
+            System.out.println(csvItems.get(i)[0] + " " + csvItems.get(i)[1] +  " " +  csvItems.get(i)[2]);
+        }
     }
 }
