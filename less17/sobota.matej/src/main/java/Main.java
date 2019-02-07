60abf47ddf6bc1437a98964ec75917e0a05102b1
Matej Sobota <matej.sobota@globallogic.com>
2019-02-07 16:32:07 +0100

    sdsa

--- less17/sobota.matej/src/main/java/Main.java
+++ less17/sobota.matej/src/main/java/Main.java
@@ -1,2 +1,15 @@
+import java.util.Arrays;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Integer[] pole = new Integer[10];
+        for (int i = 0; i < pole.length; i++) {
+            pole[i] = new Integer(i + 1);
+            pole[i] = i*3;
+
+        }
+        System.out.println(Arrays.toString(pole));
+    }
 }
