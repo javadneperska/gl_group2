4d9261225bb342f9d40df376a01aa95ae3686309
MatejPliesovsky <matej.pliesovsky@globallogic.com>
2019-02-07 13:43:02 +0100

    less16 hw finished

--- homework/less16/pliesovsky.matej/src/main/java/CSVreader.java
+++ homework/less16/pliesovsky.matej/src/main/java/CSVreader.java
@@ -2,27 +2,42 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
 
 public class CSVreader {
+
     public static ArrayList<String[]> readFile() {
         String file = "src/main/resources/humans.csv";
-        ArrayList<String[]> content = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+//        ArrayList<String[]> content = new ArrayList<>();
+        try (
+                BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line = "";
+            Human[] fieldHuman = new Human[10];
+
             while ((line = br.readLine()) != null) {
-                content.add(line.split(","));
+                String[] fieldLine = line.split(",");
+                Human human = new Human(fieldLine[0], fieldLine[1], fieldLine[2]);
+                int rL = 0;
+                fieldHuman[rL] = human;
+                rL++;
+                System.out.println(human);
             }
-            //line counter
+
+        //line counter
             long countOfLines = Files.lines(Paths.get(new File("src/main/resources/humans.csv").getPath())).count();
             System.out.println(String.format("There are %s lines in the document", countOfLines));
             System.out.println(" ");
 
+
+
+
         } catch (FileNotFoundException e) {
-            //Some error logging
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return content;
+
+
+        return null;
     }
+
 }
---
+++ homework/less16/pliesovsky.matej/src/main/java/Human.java
@@ -0,0 +1,47 @@
+public class Human {
+    private String firstName;
+    private String lastName;
+    private String DOB;
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public Human() {
+    }
+
+    public Human(String firstName, String lastName, String DOB) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.DOB = DOB;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", DOB='" + DOB + '\'' +
+                '}';
+    }
+}
