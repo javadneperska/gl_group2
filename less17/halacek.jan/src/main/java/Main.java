0e2c69cf73855321dd434be22d7709dbe9e3e282
jan.halacek <halacek@yahoo.com>
2019-02-07 16:21:03 +0100

    jh - this commit will blow you away

--- less17/halacek.jan/src/main/java/Main.java
+++ less17/halacek.jan/src/main/java/Main.java
@@ -1,14 +1,55 @@
 import java.util.Arrays;
 
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        Integer[] pole = new Integer[10];
+//
+//        for(int i = 0; i < pole.length; i++){
+//            pole[i] = i+1;
+//        }
+//
+//        System.out.println(Arrays.toString(pole));
+//    }
+//}
+
 public class Main {
     public static void main(String[] args) {
 
-        Integer[] pole = new Integer[10];
+        Integer[] secondField = {0, 1, 2, 3, 4};
+        System.out.println(Arrays.toString(secondField));
 
-        for(int i = 0; i < pole.length; i++){
-            pole[i] = i+1;
+        incrementField(secondField);
+
+        System.out.println(Arrays.toString(secondField));
+
+        int x = 5;
+        changeValue(x);
+        System.out.println("x po volani metody: " + x);
+
+
+        Person jan = new Person();
+        jan.setFirstName("j");
+        jan.setLastName("h");
+
+        modifyPerson(jan);
+        System.out.println("po volani metody: " + jan.getLastName());
+    }
+
+    public static void modifyPerson(Person jan) {
+        jan.setLastName("Halacek");
+    }
+
+    public static void changeValue(int x) {
+        x = 6;
+        System.out.println("x je: " + x);
+    }
+
+    private static void incrementField(Integer[] secondField) {
+        for (int i = 0; i < secondField.length; i++) {
+            secondField[i] = secondField[i] + 1;
         }
-
-        System.out.println(Arrays.toString(pole));
     }
 }
+//pole 1, 2, 3, 4
+//metodu, ktora ku kazdemu prvku pola pripocita 1
