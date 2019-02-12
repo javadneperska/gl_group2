2b1f25232c1bce78a9e096cf7e218f58672b3bc6
miroslav.katrak <miroslav.katrak@gmail.com>
2019-02-12 17:25:59 +0100

    extends

--- less18/katrak.miroslav/src/main/java/sk/mka/gl/group2/less18/geometry/Rectangle.java
+++ less18/katrak.miroslav/src/main/java/sk/mka/gl/group2/less18/geometry/Rectangle.java
@@ -1,10 +1,14 @@
 package sk.mka.gl.group2.less18.geometry;
 
-public class Rectangle {
+public  class Rectangle {
     protected double width;
     protected double height;
 
     public Rectangle() {
+    }
+
+    public Rectangle(double height) {
+        this.height = height;
         System.out.println("Rectangle");
     }
 
@@ -20,11 +24,11 @@ public class Rectangle {
         return height;
     }
 
-    public void setHeight(double height) {
+    public final void setHeight(double height) {
         this.height = height;
     }
 
-    public double content() {
+    public  double content() {
         return this.width * this.height;
     }
 }
